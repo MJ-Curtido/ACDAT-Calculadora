5054047ae9f3be45098fe4fc62d3f7e234705b65
Mxnuu7_ <manu17pc@gmail.com>
2022-10-16 23:53:18 +0200

    V1

--- app/src/main/java/com/example/acdat_calculadora/MainActivity.java
+++ app/src/main/java/com/example/acdat_calculadora/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.acdat_calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.acdat_calculadora.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public ActivityMainBinding getBinding() {
+        return binding;
     }
 }
